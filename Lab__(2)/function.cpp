751fa4377ed8c0cafb2d632094f96e6d82bb5161
Данила Абрамчук <abramchukdanila@gmail.com>
2025-09-23 12:55:07 +0300

    commit 22

--- Lab__(2)/function.cpp
+++ Lab__(2)/function.cpp
@@ -33,45 +33,6 @@ String::~String() {
     delete[] text;
 }
 
-String &String::operator=(const String &other) {
-    if (this != &other) {
-        delete[] text;
-        size_t len = 0;
-        while (other.text[len] != '\0') ++len;
-        text = new char[len + 1];
-        for (size_t i = 0; i < len; ++i)
-            text[i] = other.text[i];
-        text[len] = '\0';
-    }
-    return *this;
-}
-
-String &String::operator+=(const String &other) {
-    size_t len1 = 0;
-    while (text[len1] != '\0') ++len1;
-    size_t len2 = 0;
-    while (other.text[len2] != '\0') ++len2;
-    auto temp = new char[len1 + len2 + 1];
-    for (size_t i = 0; i < len1; ++i)
-        temp[i] = text[i];
-    for (size_t i = 0; i < len2; ++i)
-        temp[len1 + i] = other.text[i];
-    temp[len1 + len2] = '\0';
-    delete[] text;
-    text = temp;
-    return *this;
-}
-
-bool String::operator==(const String &other) const {
-    size_t i = 0;
-    while (text[i] != '\0' && other.text[i] != '\0') {
-        if (text[i] != other.text[i])
-            return false;
-        ++i;
-    }
-    return text[i] == other.text[i];
-}
-
 void ShowMenu() {
     String s1;
     String s2;
--- Lab__(2)/function.h
+++ Lab__(2)/function.h
@@ -16,9 +16,34 @@ public:
 
     ~String();
 
-    String &operator=(const String &other);
+    String &operator=(const String &other){
+        if (this != &other) {
+            delete[] text;
+            size_t len = 0;
+            while (other.text[len] != '\0') ++len;
+            text = new char[len + 1];
+            for (size_t i = 0; i < len; ++i)
+                text[i] = other.text[i];
+            text[len] = '\0';
+        }
+        return *this;
+    }
 
-    String &operator+=(const String &other);
+    String &operator+=(const String &other){
+        size_t len1 = 0;
+        while (text[len1] != '\0') ++len1;
+        size_t len2 = 0;
+        while (other.text[len2] != '\0') ++len2;
+        auto temp = new char[len1 + len2 + 1];
+        for (size_t i = 0; i < len1; ++i)
+            temp[i] = text[i];
+        for (size_t i = 0; i < len2; ++i)
+            temp[len1 + i] = other.text[i];
+        temp[len1 + len2] = '\0';
+        delete[] text;
+        text = temp;
+        return *this;
+    }
 
     bool operator==(const String &other) const {
         size_t i = 0;
@@ -28,6 +53,15 @@ public:
             ++i;
         }
         return text[i] == '\0' && other.text[i] == '\0';
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const String &s) {
+        size_t i = 0;
+        while (s.text[i] != '\0') {
+            out << s.text[i];
+            ++i;
+        }
+        return out;
     }
 
     friend std::istream &operator>>(std::istream &in, String &s) {
