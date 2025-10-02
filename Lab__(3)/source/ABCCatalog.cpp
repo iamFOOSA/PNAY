e768f72be98ffe466a0ae5e95efce55986244268
Данила Абрамчук <abramchukdanila@gmail.com>
2025-10-02 23:32:47 +0300

    commit 1321

--- Lab__(3)/source/ABCCatalog.cpp
+++ Lab__(3)/source/ABCCatalog.cpp
@@ -9,7 +9,13 @@ ABCCatalog::~ABCCatalog() {
 void ABCCatalog::show_all_sorted() const {
     cout << "Все карточки в алфавитном порядке:" << endl;
 
-    auto *titles = new string[card_count];
+    if (card_count == 0) {
+        cout << "Каталог пуст." << endl;
+        return;
+    }
+
+    auto titles = new string[card_count];
+
     for (int i = 0; i < card_count; i++) {
         titles[i] = cards[i]->get_alphabetical_search();
     }
@@ -17,15 +23,13 @@ void ABCCatalog::show_all_sorted() const {
     for (int i = 0; i < card_count - 1; i++) {
         for (int j = 0; j < card_count - i - 1; j++) {
             if (titles[j] > titles[j + 1]) {
-                string temp = titles[j];
-                titles[j] = titles[j + 1];
-                titles[j + 1] = temp;
+                swap(titles[j], titles[j + 1]);
             }
         }
     }
 
     for (int i = 0; i < card_count; i++) {
-        cout << "  " << (i + 1) << ". " << titles[i] << endl;
+        cout << titles[i] << endl;
     }
 
     delete[] titles;
