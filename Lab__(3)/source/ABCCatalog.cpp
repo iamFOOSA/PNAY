394294e2c3d0400fe933b94af675c7820199b9af
Данила Абрамчук <abramchukdanila@gmail.com>
2025-10-03 00:58:33 +0300

    commit 11251

--- Lab__(3)/source/ABCCatalog.cpp
+++ Lab__(3)/source/ABCCatalog.cpp
@@ -5,30 +5,32 @@ void ABCCatalog::show_all_sorted() const {
     std::cout << "Все карточки в алфавитном порядке:" << std::endl;
     int count = get_card_count();
 
-    if (count <= 0) {
+    if (count == 0) {
         std::cout << "Каталог пуст." << std::endl;
         return;
     }
 
-    if (count > 0) {
-        auto titles = new std::string[count];
+    auto titles = new std::string[count];
 
-        for (int i = 0; i < count; i++) {
-            titles[i] = get_card(i)->get_alphabetical_search();
-        }
+    for (int i = 0; i < count; i++) {
+        titles[i] = get_card(i)->get_alphabetical_search();
+    }
 
-        for (int i = 0; i < count - 1; i++) {
-            for (int j = 0; j < count - i - 1; j++) {
-                if (titles[j] > titles[j + 1]) {
-                    std::swap(titles[j], titles[j + 1]);
-                }
+    bool swapped;
+    for (int i = 0; i < count - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < count - i - 1; j++) {
+            if (titles[j] > titles[j + 1]) {
+                std::swap(titles[j], titles[j + 1]);
+                swapped = true;
             }
         }
-
-        for (int i = 0; i < count; i++) {
-            std::cout << titles[i] << std::endl;
-        }
-
-        delete[] titles;
+        if (!swapped) break;
     }
+
+    for (int i = 0; i < count; i++) {
+        std::cout << titles[i] << std::endl;
+    }
+
+    delete[] titles;
 }
