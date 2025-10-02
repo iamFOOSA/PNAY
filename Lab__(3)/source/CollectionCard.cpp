5f7f39aa882cb56cd175b6f24142ea0ee17cf1bc
Данила Абрамчук <abramchukdanila@gmail.com>
2025-10-02 23:59:19 +0300

    commit 181

--- Lab__(3)/source/CollectionCard.cpp
+++ Lab__(3)/source/CollectionCard.cpp
@@ -16,7 +16,11 @@ CollectionCard::~CollectionCard() {
 
 void CollectionCard::add_article(Article *article) {
     if (article_count >= capacity) {
-        int new_capacity = (capacity == 0) ? 2 : capacity * 2;
+        int new_capacity = std::max(2, capacity * 2);
+        if (new_capacity <= article_count) {
+            new_capacity = article_count + 1;
+        }
+
         auto new_articles = new Article *[new_capacity];
 
         for (int i = 0; i < article_count; i++) {
