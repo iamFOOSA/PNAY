30b270638750e8db123ccf25e05d405cf59e3ac4
Данила Абрамчук <abramchukdanila@gmail.com>
2025-10-29 21:00:42 +0300

    commit 5_11

--- Labs__(5)/header/Tree.h
+++ Labs__(5)/header/Tree.h
@@ -48,8 +48,24 @@ private:
         }
     }
 
+    TreeNode<T>* copy_tree(const TreeNode<T>* node) {
+        if (node == nullptr) {
+            return nullptr;
+        }
+        auto* new_node = new TreeNode<T>(node->get_data());
+        new_node->set_left_node(copy_tree(node->get_left_node()));
+        new_node->set_right_node(copy_tree(node->get_right_node()));
+        return new_node;
+    }
+
 public:
     Tree() = default;
+
+    Tree(const Tree& other) {
+        if (other.root != nullptr) {
+            root = copy_tree(other.root);
+        }
+    }
 
     ~Tree() {
         delete_tree(root);
