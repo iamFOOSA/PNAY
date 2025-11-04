81762c154e3a97b54f9e64c75e1dbebfe066aac7
Данила Абрамчук <abramchukdanila@gmail.com>
2025-11-04 12:51:13 +0300

    commit 6_31

--- Lab__(6)/source/main.cpp
+++ Lab__(6)/source/main.cpp
@@ -116,17 +116,16 @@ void access_by_index() {
     std::cout << "Выберите строку (1 - первая, 2 - вторая): ";
     std::cin >> string_choice;
 
-    String *const selected_str = (string_choice == 1) ? str1 :
-                                 (string_choice == 2) ? str2 : nullptr;
-
-    std::string str_name;
+    String *selected_str_temp = nullptr;
     if (string_choice == 1) {
-        str_name = "первая";
+        selected_str_temp = str1;
     } else if (string_choice == 2) {
-        str_name = "вторая";
-    } else {
-        str_name = "";
+        selected_str_temp = str2;
     }
+    String *const selected_str = selected_str_temp;
+
+    std::string str_name = (string_choice == 1) ? "первая" :
+                           (string_choice == 2) ? "вторая" : "";
 
     if (selected_str == nullptr) {
         std::cout << "Ошибка: " << (str_name.empty() ? "Неверный выбор!" : str_name + " строка не создана!") << "\n";
